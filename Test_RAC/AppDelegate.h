6ebd37755dae7fc70338b7bcc70055b95d52c910
liudukun <ldk@liudukun.local>
2016-06-01 10:22:39 +0800

    Initial Commit

---
+++ Test_RAC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_RAC
+//
+//  Created by liudukun on 16/6/1.
+//  Copyright © 2016年 liudukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test_RAC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test_RAC
+//
+//  Created by liudukun on 16/6/1.
+//  Copyright © 2016年 liudukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
